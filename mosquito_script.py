8568df1295683a1ef6b1a0248afb1d3b6e4e87f9
Yan Song <yanssbu@ucsd.edu>
2014-10-21 14:21:59 -0700

    get filename from command line

--- mosquito_script.py
+++ mosquito_script.py
@@ -1,15 +1,21 @@
+import sys # to interface with system
 import pandas as pd
 import analyze_mosquito_data as am
 
-filename = 'A1_mosquito_data.csv'
-data = pd.read_csv(filename)
+filename = sys.argv[1] # 1st argument
 
+print 'Analyzing', filename
+
+data = pd.read_csv(filename)
 
 data['temperature'] = am.change_to_celsius(data['temperature'])
 
-
-parameters = am.analyze(data, 'plot.png')
+print 'Running analyze'
+parameters = am.analyze(data, filename.replace('csv','png'))
 # if the function is defined with 2 parameters, when the function is called, 2 parameters have to be called
+# arguments can be called by positions or by keyname eg figure_filename='plot.png'
 
 # save parameters to file
-parameters.to_csv('parameters.csv')
+
+print 'Saving parameters'
+parameters.to_csv(filename.replace('data', 'parameters'))
